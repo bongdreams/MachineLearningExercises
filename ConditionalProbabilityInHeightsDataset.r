cf742ae6858ef243af41defcdfc733370d9be8ec
Saikat Banerjee <banerjeesaikat6@gmail.com>
2018-12-24 22:24:54 +0530

    Update ConditionalProbabilityInHeightsDataset.r

--- ConditionalProbabilityInHeightsDataset.r
+++ ConditionalProbabilityInHeightsDataset.r
@@ -12,9 +12,7 @@ qplot(height, p, data =.)
 
 ps <- seq(0, 1, 0.1)
 heights %>% 
-  
   mutate(g = cut(height, quantile(height, ps), include.lowest = TRUE)) %>%
-  
 group_by(g) %>%
   summarize(p = mean(sex == "Male"), height = mean(height)) %>%
   qplot(height, p, data =.)
