1187b704e882a2f57268c7bf52cd4a1e42ea81e1
bongdreams <banerjeesaikat6@gmail.com>
2018-12-28 01:45:19 +0530

    getting accuracy after getting the statistically significant data points

--- k-fold_Cross_Validation_ttest.r
+++ k-fold_Cross_Validation_ttest.r
@@ -9,8 +9,13 @@ x <- matrix(rnorm(n*p), n, p)
 colnames(x) <- paste("x", 1:ncol(x), sep = "_")
 y <- rbinom(n, 1, 0.5)
 
-x_subset <- x[ ,sample(p, 100)]
-
 tt <- colttests(x, as.factor(y))
 
-tt %>% filter(p.value < 0.01)
+tt %>% filter(p.value <= 0.01)
+
+ind <- which(tt$p.value <= 0.01)
+
+x_subset <- x[,ind]
+
+fit <- train(x_subset, y, method = "glm")
+fit$results
