b3aebe73fdc9d742a8e2ee2f56d55a40e5b9b9f5
bongdreams <banerjeesaikat6@gmail.com>
2018-12-28 03:12:47 +0530

    resampling instead of montecarlo

---
+++ Estimation_Using_bootstrap_resampling.r
@@ -0,0 +1,19 @@
+
+y <- rnorm(100, 0, 1)
+
+set.seed(1)
+indexes <- createResample(y, 10000)
+
+output <- matrix(ncol=1, nrow=10000)
+
+count <- 1
+for(i in indexes){
+  output[count,] <- quantile(i, 0.75)
+  count <- count +1 
+  
+}
+
+q_star <- data.frame(output)
+mean(q_star$output)
+
+sd(q_star$output)
--- monte_carlo_on_quantile.r
+++ monte_carlo_on_quantile.r
@@ -3,7 +3,9 @@ y <- rnorm(100, 0, 1)
 set.seed(1)
 Q_stars <- replicate(B, {
   y_star <- rnorm(100, 0, 1)
+  print(y_star)
   Q_star <- quantile(y_star, 0.75)
+  
 })
 mean(Q_stars)
 sd(Q_stars)
