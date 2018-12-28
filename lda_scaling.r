b1874f8314dc1f20a10f9194ada509e183a50d0c
bongdreams <banerjeesaikat6@gmail.com>
2018-12-29 01:34:25 +0530

    lda by using pre-processing to scale the points for the finalModel means

---
+++ lda_scaling.r
@@ -0,0 +1,18 @@
+library(caret)
+library(dslabs)
+library(ggplot2)
+library(dplyr)
+set.seed(1993)
+data("tissue_gene_expression")
+ind <- which(tissue_gene_expression$y %in% c("cerebellum", "hippocampus"))
+y <- droplevels(tissue_gene_expression$y[ind])
+x <- tissue_gene_expression$x[ind, ]
+x <- x[, sample(ncol(x), 10)]
+
+train_lda<-train(x, y, method="lda",data=x, preProcess = "center")
+
+train_lda
+
+train_lda$finalModel$means
+
+qplot(train_lda$finalModel$means[1,], train_lda$finalModel$means[2,])
