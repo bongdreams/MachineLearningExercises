c6a14569e07e709c62f1d9e18a230ae4f879fcb4
bongdreams <banerjeesaikat6@gmail.com>
2018-12-30 00:34:21 +0530

    regression trees

---
+++ regression_tree_on_tissuegeneexpression.r
@@ -0,0 +1,18 @@
+library(caret)
+library(rpart)
+library(rpart.plot)
+data("tissue_gene_expression")
+
+
+x <- tissue_gene_expression$x
+y <- tissue_gene_expression$y
+set.seed(1991)
+#fit <- train(x, y, method="rpart", control = rpart.control(minsplit = 0))
+
+fittest <- rpart(y ~ x, data = tissue_gene_expression, control = rpart.control(cp = seq(0, 0.1, 0.01)))
+
+y_hat <- predict(fittest, type = "class")
+
+confusionMatrix(data = as.factor(y_hat), reference = as.factor(tissue_gene_expression$y))
+
+rpart.plot(fittest)
