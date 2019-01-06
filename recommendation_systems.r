7aa8a395dc93770e26ab2f52cc43d570517a0fb6
bongdreams <banerjeesaikat6@gmail.com>
2019-01-07 02:28:13 +0530

    Creating the rmse function

--- recommendation_systems.r
+++ recommendation_systems.r
@@ -31,36 +31,5 @@ rmse_results <- data.frame(method = "Just the average", RMSE = naive_rmse)
 # fit <- lm(rating ~ as.factor(movieId), data=movielens)
 # plot(fit)
 
-mu <- mean(train_set$rating)
-movie_avgs <- train_set %>% group_by(movieId) %>% summarize(b_i = mean(rating - mu))
-
-hist(movie_avgs$b_i)
-
-predicted_ratings <- mu + test_set %>% left_join(movie_avgs, by = 'movieId') %>% .$b_1
-
-model_1_rmse <- RMSE(predicted_ratings, test_set$rating)
-
-rmse_results <- bind_rows(rmse_results, data.frame(method='Movie effect model', RMSE = model_1_rmse))
-rmse_results %>% knitr::kable()
-
-
-# Bringing users into the mix
-# fit <- lm(rating ~ as.factor(userId) + as.factor(movieId), data=movielens)
-# plot(fit)
-
-user_avgs <- test_set %>% 
-  left_join(movie_avgs, by = 'movieId') %>% 
-  group_by(userId) %>% 
-  summarize(b_u = mean(rating - mu - b_i))
-
-predicted_ratings <- test_set %>% 
-  left_join(movie_avgs, by = 'movieId') %>% 
-  left_join(user_avgs, by ='userId') %>% mutate(pred = mu + b_i + b_u) %>% .$pred
-
-model_2_rmse <- RMSE(predicted_ratings, test_set$rating)
-
-rmse_results <- bind_rows(rmse_results, data.frame(method = 'Movie + User Effects Model', RMSE = model_2_rmse))
-
-rmse_results %>% knitr::kable()
 
 
